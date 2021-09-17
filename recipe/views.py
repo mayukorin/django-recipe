257cac7f164abd19978c800871161d80b799e17d
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-18 02:12:56 +0900

    Add random view

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from .models import Recipe
 # Create your views here.
+
+class RandomRecipeView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        random_recipe_list = Recipe.objects.all
+        context = {
+            'random_recipe_list' : random_recipe_list
+        }
+        
+        return render(request, 'recipe/random.html', context)
