b645f54ca51dc83c5ab14ac2de8349970882b9e0
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-09-20 00:25:18 +0900

    Add base.html

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+app_name = 'recipe'
 urlpatterns = [
-    path('random', views.RandomRecipeView.as_view()), 
+    path('random', views.RandomRecipeView.as_view(), name='random'), 
 ]
