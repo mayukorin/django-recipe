ccb09625db099478d99f1b67fbcad3b9b1b9ead1
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-10-06 23:26:07 +0900

    Change function base to class base of views.py

--- recipe/urls.py
+++ recipe/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('result_for_ingredient', views.ResultRecipeForIngredientView.as_view(), name='ingredient_result'),
     path('siteUser/register', views.SiteUserRegisterView.as_view(), name='site_user_register'),
     path('siteUser/login', views.SiteUserLoginView.as_view(), name='site_user_login'),
-    path('make_favorite/', views.MakeFavorite, name='make_favorite'),
-    path('destroy_favorite/', views.DestroyFavorite, name='destroy_favorite'),
-    path('my_recipe', views.MyRecipeIndexView.as_view(), name='my_recipe'),
+    path('make_favorite/', views.MakeFavoriteView.as_view(), name='make_favorite'),
+    path('destroy_favorite/', views.DestroyFavoriteView.as_view(), name='destroy_favorite'),
+    path('favorite_recipe', views.FavoriteRecipeIndexView.as_view(), name='favorite_recipe'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,107 +1,85 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from .models import Recipe, Category, Ingredient, SiteUser
+from .models import Recipe, Category
 from .forms import SiteUserRegisterForm, SiteUserLoginForm
 from django.contrib.auth import login as auth_login
+import json
+from django.http import HttpResponse
+
 # Create your views here.
 
 
 class RandomRecipeView(View):
-
     def get(self, request, *args, **kwargs):
 
-        random_recipes = Recipe.objects.order_by('?')[:5]
-        context = {
-            'random_recipes' : random_recipes
-        }
-        
-        return render(request, 'recipe/random.html', context)
+        random_recipes = Recipe.objects.order_by("?")[:5]
+        context = {"random_recipes": random_recipes}
+
+        return render(request, "recipe/random.html", context)
 
 
 class SearchRecipeForIngredientView(View):
-
     def get(self, request, *args, **kwargs):
-        
-        search_categories = Category.objects.all().prefetch_related('ingredients')
-        context = { 'search_categories': search_categories }
-        return render(request, 'recipe/search_for_ingredient.html', context)
+
+        search_categories = Category.objects.all().prefetch_related("ingredients")
+        context = {"search_categories": search_categories}
+        return render(request, "recipe/search_for_ingredient.html", context)
 
 
 class ResultRecipeForIngredientView(View):
-
     def get(self, request, *args, **kwargs):
-        
-        ingredient_id_list = self.request.GET.getlist('ingredients')
+
+        ingredient_id_list = self.request.GET.getlist("ingredients")
         result_recipes = Recipe.objects.filter()
         for ingredient_id in ingredient_id_list:
-            result_recipes =result_recipes.filter(ingredients=ingredient_id)
-        
+            result_recipes = result_recipes.filter(ingredients=ingredient_id)
+
         result_recipes = result_recipes.values()
         for recipe in result_recipes:
-            recipe['favorite_flag'] = request.user.favorite_recipes.filter(pk=recipe['id']).exists()
-           
-        print(result_recipes)
-        context = { 'result_recipes' : result_recipes }
-        
-        return render(request, 'recipe/search_result.html', context)
+            recipe["favorite_flag"] = request.user.is_favorite_recipe(recipe["id"])
 
-# レシピのお気に入り登録
+        context = {"result_recipes": result_recipes}
 
-class FavoriteRecipeRegisterView(View):
-
-    def get(self, request, *args, **kwargs):
-
-        favorite_recipe = Recipe.objects.get(id=self.request.GET['recipe_id'])
-        request.user.favorite_recipes.add(favorite_recipe)
-
-        return redirect("recipe:ingredient_result")
+        return render(request, "recipe/search_result.html", context)
 
 
-class MyRecipeIndexView(View):
-
+class FavoriteRecipeIndexView(View):
     def get(self, request, *args, **kwargs):
 
         favorite_recipes = request.user.favorite_recipes.all().values()
         for recipe in favorite_recipes:
-            recipe['favorite_flag'] = True
-        context = { 'my_recipes': favorite_recipes }
+            recipe["favorite_flag"] = True
+        context = {"favorite_recipes": favorite_recipes}
 
-        return render(request, 'recipe/my_recipe.html', context)
-
-
-
-def MakeFavorite(request):
-
-    import json
-    from django.http import HttpResponse
-    from .models import Recipe, SiteUser
-
-    favorite_recipe = Recipe.objects.get(id=request.POST['recipe_id'])
-    request.user.favorite_recipes.add(favorite_recipe)
-
-    response_data = {}
-    response_data["recipe_id"] = request.POST['recipe_id']
-    
-    json = json.dumps(response_data)
-    print(json)
-    return HttpResponse(json, content_type='application/json')
+        return render(request, "recipe/favorite_recipe.html", context)
 
 
-def DestroyFavorite(request):
+class MakeFavoriteView(View):
+    def post(self, request, *args, **kwargs):
 
-    import json
-    from django.http import HttpResponse
-    from .models import Recipe, SiteUser
+        if request.is_ajax():
+            favorite_recipe = Recipe.objects.get(id=request.POST["recipe_id"])
+            request.user.favorite_recipes.add(favorite_recipe)
 
-    favorite_recipe = Recipe.objects.get(id=request.POST['recipe_id'])
-    request.user.favorite_recipes.remove(favorite_recipe)
+            response_data = {}
+            response_data["recipe_id"] = request.POST["recipe_id"]
 
-    response_data = {}
-    response_data["recipe_id"] = request.POST['recipe_id']
-    
-    json = json.dumps(response_data)
-    print(json)
-    return HttpResponse(json, content_type='application/json')
+            json_response = json.dumps(response_data)
+            return HttpResponse(json_response, content_type="application/json")
+
+
+class DestroyFavoriteView(View):
+    def post(self, request, *args, **kwargs):
+
+        if request.is_ajax():
+            favorite_recipe = Recipe.objects.get(id=request.POST["recipe_id"])
+            request.user.favorite_recipes.remove(favorite_recipe)
+
+            response_data = {}
+            response_data["recipe_id"] = request.POST["recipe_id"]
+
+            json_response = json.dumps(response_data)
+            return HttpResponse(json_response, content_type="application/json")
 
 
 # 会員登録
@@ -111,7 +89,6 @@ class SiteUserRegisterView(View):
             "form": SiteUserRegisterForm(),
         }
         return render(request, "recipe/siteUser/register.html", context)
-
 
     def post(self, request, *args, **kwargs):
         form = SiteUserRegisterForm(request.POST)
@@ -124,7 +101,7 @@ class SiteUserRegisterView(View):
         new_site_user.save()
         # messages.success(request, "会員登録が完了しました")
         # return redirect("app:site_user_login")
-    
+
 
 # ログイン
 class SiteUserLoginView(View):
