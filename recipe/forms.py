49ef2a570c61e6ab05f309fa4b8c18efb62f268f
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-11-12 00:05:54 +0900

    apply black to views and forms

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,56 +1,7 @@
 from django import forms
 from .models import SiteUser
-from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
-from django.core.exceptions import ValidationError
 
-class SiteUserRegisterForm(forms.ModelForm):
-    class Meta:
-        model = SiteUser
-
-        fields = (
-            "username",
-            "email",
-            "password",
-        )
-
-        labels = {
-            "password": "パスワード",
-        }
-
-        widgets = {
-            "username": forms.TextInput(),
-            "password": forms.PasswordInput(),
-        }
-
-        error_messages = {
-            "username": {
-                "required" : "ユーザ名を入力してください",
-                "max_length" : "名前は150字以内で入力してください"
-            },
-            "password": {
-                "required" : "パスワードを入力してください"
-            },
-            "email" : {
-                "required" : "メールアドレスを入力してください",
-                "unique" : "そのメールアドレスは既に使われています",                                  
-                "invalid" : "メールアドレスは正しい形式で入力してください"
-            }
-        }
-
-    password2 = forms.CharField(
-        label="確認用パスワード", required=True, error_messages={'required': '確認用パスワードを入力してください'}, widget=forms.PasswordInput(),
-    )
-
-
-    def clean(self):
-
-        password = self.cleaned_data.get("password")
-        password2 = self.cleaned_data.get("password2")
-        if password != password2:
-            raise forms.ValidationError("パスワードと確認用パスワードが一致しません")
-        # ユニーク制約を自動でバリデーション
-        super().clean()
 
 class SignUpForm(forms.ModelForm):
     class Meta:
@@ -73,23 +24,23 @@ class SignUpForm(forms.ModelForm):
 
         error_messages = {
             "username": {
-                "required" : "ユーザ名を入力してください",
-                "max_length" : "名前は150字以内で入力してください"
+                "required": "ユーザ名を入力してください",
+                "max_length": "名前は150字以内で入力してください",
             },
-            "password": {
-                "required" : "パスワードを入力してください"
+            "password": {"required": "パスワードを入力してください"},
+            "email": {
+                "required": "メールアドレスを入力してください",
+                "unique": "そのメールアドレスは既に使われています",
+                "invalid": "メールアドレスは正しい形式で入力してください",
             },
-            "email" : {
-                "required" : "メールアドレスを入力してください",
-                "unique" : "そのメールアドレスは既に使われています",                                  
-                "invalid" : "メールアドレスは正しい形式で入力してください"
-            }
         }
 
     password2 = forms.CharField(
-        label="確認用パスワード", required=True, error_messages={'required': '確認用パスワードを入力してください'}, widget=forms.PasswordInput(),
+        label="確認用パスワード",
+        required=True,
+        error_messages={"required": "確認用パスワードを入力してください"},
+        widget=forms.PasswordInput(),
     )
-
 
     def clean(self):
 
@@ -110,117 +61,70 @@ class UserPropertyChangeForm(forms.ModelForm):
             "email",
         )
 
-
         widgets = {
             "username": forms.TextInput(),
         }
 
         error_messages = {
             "username": {
-                "required" : "ユーザ名を入力してください",
-                "max_length" : "名前は150字以内で入力してください"
+                "required": "ユーザ名を入力してください",
+                "max_length": "名前は150字以内で入力してください",
             },
-            "email" : {
-                "required" : "メールアドレスを入力してください",
-                "unique" : "そのメールアドレスは既に使われています",                                  
-                "invalid" : "メールアドレスは正しい形式で入力してください"
-            }
+            "email": {
+                "required": "メールアドレスを入力してください",
+                "unique": "そのメールアドレスは既に使われています",
+                "invalid": "メールアドレスは正しい形式で入力してください",
+            },
         }
+
 
 class PasswordEditForm(PasswordChangeForm):
 
-    
     new_password1 = forms.CharField(
-        label= '新しいパスワード',
+        label="新しいパスワード",
         strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}),
-        error_messages={
-            'required' : '新しいパスワードを入力してください',
-        }
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password"}),
+        error_messages={"required": "新しいパスワードを入力してください",},
     )
 
     new_password2 = forms.CharField(
-        label= '確認用パスワード',
+        label="確認用パスワード",
         strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}),
-        error_messages={
-            'required' : '確認用パスワードを入力してください',
-        }
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password"}),
+        error_messages={"required": "確認用パスワードを入力してください",},
     )
 
     old_password = forms.CharField(
-        label= '現在のパスワード',
+        label="現在のパスワード",
         strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}),
-        error_messages={
-            'required' : '現在のパスワードを入力してください',
-        }
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password"}),
+        error_messages={"required": "現在のパスワードを入力してください",},
     )
 
     error_messages = {
-        'password_mismatch' : '新しいパスワードと確認用パスワードが一致しません',
-        'password_incorrect': '現在のパスワードが正しくありません',
-    } 
-
-
-
-class SiteUserLoginForm(forms.Form):
-
-    email = forms.EmailField(label="メールアドレス", 
-                            error_messages={
-                                'required': 'メールアドレスを入力してください',
-                                "invalid" : "メールアドレスは正しい形式で入力してください"
-                            })
-    password = forms.CharField(
-        label="パスワード", widget=forms.PasswordInput(),
-        error_messages={
-            "required" : "パスワードを入力してください"
-        }
-    )
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.site_user_cache = None
-
-    def clean(self):
-        email = self.cleaned_data.get("email")
-        password = self.cleaned_data.get("password")
-
-        try:
-            site_user = SiteUser.objects.get(email=email)
-        except ObjectDoesNotExist:
-            raise forms.ValidationError("メールアドレスかパスワードが間違っています")
-        if not site_user.check_password(password):
-            raise forms.ValidationError("メールアドレスかパスワードが間違っています")
-
-        self.site_user_cache = site_user
-
-    def get_site_user(self):
-        return self.site_user_cache
+        "password_mismatch": "新しいパスワードと確認用パスワードが一致しません",
+        "password_incorrect": "現在のパスワードが正しくありません",
+    }
 
 
 class SignInForm(AuthenticationForm):
 
     username = forms.EmailField(
-        widget=forms.TextInput(attrs={'autofocus': True}),
+        widget=forms.TextInput(attrs={"autofocus": True}),
         error_messages={
-            'required': 'メールアドレスを入力してください',
-            'invalid' : 'メールアドレスは正しい形式で入力してください'
-        }
-        
+            "required": "メールアドレスを入力してください",
+            "invalid": "メールアドレスは正しい形式で入力してください",
+        },
     )
 
     password = forms.CharField(
-        label= 'パスワード',
+        label="パスワード",
         strip=False,
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}),
-        error_messages={
-            'required' : 'パスワードを入力してください',
-        }
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password"}),
+        error_messages={"required": "パスワードを入力してください",},
     )
 
     error_messages = {
-        'invalid_login' : 'メールアドレスかパスワードが間違っています',
-        'inactive' : 'アカウントが有効ではありません',
+        "invalid_login": "メールアドレスかパスワードが間違っています",
+        "inactive": "アカウントが有効ではありません",
     }
-
--- recipe/views.py
+++ recipe/views.py
@@ -1,22 +1,21 @@
 from django.db.models.query_utils import FilteredRelation
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import View
-from .models import Ingredient, Recipe, Category, SiteUser
+from .models import Recipe, Category, SiteUser
 from .forms import SignInForm, SignUpForm, UserPropertyChangeForm, PasswordEditForm
-from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth import login as auth_login
 import json
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView as AuthLoginView, PasswordChangeView as AuthPasswordChangeView, PasswordChangeDoneView as AuthPasswordChangeDoneView
+from django.contrib.auth.views import (
+    LoginView as AuthLoginView,
+    PasswordChangeView as AuthPasswordChangeView,
+)
 from django.contrib.auth.views import LogoutView as AuthLogoutView
 from django.views.generic import CreateView, ListView, UpdateView
 from django.db.models import Q, FilteredRelation
 from django.db.models import Count
-from django.views.decorators.cache import never_cache
-from django.utils.decorators import method_decorator
-
 
 
 # Create your views here.
@@ -25,8 +24,8 @@ from django.utils.decorators import method_decorator
 class RandomRecipeView(ListView):
 
     model = Recipe
-    context_object_name = 'random_recipes'
-    template_name = 'recipe/random_recipe.html'
+    context_object_name = "random_recipes"
+    template_name = "recipe/random_recipe.html"
     paginate_by = 5
 
     def get(self, request, *args, **kwargs):
@@ -35,24 +34,28 @@ class RandomRecipeView(ListView):
 
     def get_queryset(self):
 
-        queryset = Recipe.objects.annotate(favorite_login_user=FilteredRelation('favorite_users', condition=Q(favorite_users__pk=self.user_pk)), favorite_flag=Count('favorite_login_user')).order_by("?")
+        queryset = Recipe.objects.annotate(
+            favorite_login_user=FilteredRelation(
+                "favorite_users", condition=Q(favorite_users__pk=self.user_pk)
+            ),
+            favorite_flag=Count("favorite_login_user"),
+        ).order_by("?")
         return queryset
-        
 
 
 class SearchRecipeForIngredientView(ListView):
-    
+
     model = Category
     queryset = Category.objects.all().prefetch_related("ingredients")
-    context_object_name = 'search_categories'
-    template_name = 'recipe/search_for_ingredient.html'
+    context_object_name = "search_categories"
+    template_name = "recipe/search_for_ingredient.html"
 
 
 class ResultRecipeForIngredientView(ListView):
 
     model = Recipe
-    context_object_name = 'result_recipes'
-    template_name = 'recipe/search_result_recipe.html'
+    context_object_name = "result_recipes"
+    template_name = "recipe/search_result_recipe.html"
     paginate_by = 5
 
     def get(self, request, *args, **kwargs):
@@ -65,37 +68,36 @@ class ResultRecipeForIngredientView(ListView):
         for ingredient_id in self.ingredient_id_list:
             queryset = queryset.filter(ingredients=ingredient_id)
 
-        queryset = queryset.annotate(favorite_login_user=FilteredRelation('favorite_users', condition=Q(favorite_users__pk=self.user_pk)), favorite_flag=Count('favorite_login_user'))
+        queryset = queryset.annotate(
+            favorite_login_user=FilteredRelation(
+                "favorite_users", condition=Q(favorite_users__pk=self.user_pk)
+            ),
+            favorite_flag=Count("favorite_login_user"),
+        )
         return queryset
 
     def get_context_data(self, **kwargs):
-    
+
         context = super().get_context_data(**kwargs)
         query_param = "&"
         for ingredient_id in self.ingredient_id_list:
             query_param += "ingredients=" + ingredient_id + "&"
-        context.update({
-            'query_param': query_param
-        })
+        context.update({"query_param": query_param})
         return context
-    
 
 
 class FavoriteRecipeIndexView(LoginRequiredMixin, ListView):
 
-    # permission_denied_message = "ログインしてください"
     model = Recipe
-    context_object_name = 'favorite_recipes'
-    template_name = 'recipe/favorite_recipe.html'
+    context_object_name = "favorite_recipes"
+    template_name = "recipe/favorite_recipe.html"
     paginate_by = 5
-
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.error(request, "ログインしてください", extra_tags='danger')
+            messages.error(request, "ログインしてください", extra_tags="danger")
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
-
 
     def get(self, request, *args, **kwargs):
         self.user_pk = request.user.pk if request.user.is_authenticated else 0
@@ -103,18 +105,19 @@ class FavoriteRecipeIndexView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
 
-        queryset = Recipe.objects.annotate(favorite_login_user=FilteredRelation('favorite_users', condition=Q(favorite_users__pk=self.user_pk)), favorite_flag=Count('favorite_login_user')).filter(favorite_users__pk=self.user_pk)
+        queryset = Recipe.objects.annotate(
+            favorite_login_user=FilteredRelation(
+                "favorite_users", condition=Q(favorite_users__pk=self.user_pk)
+            ),
+            favorite_flag=Count("favorite_login_user"),
+        ).filter(favorite_users__pk=self.user_pk)
         return queryset
 
 
-   
-
-
 class MakeFavoriteView(LoginRequiredMixin, View):
-
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.error(request, "ログインしてください", extra_tags='danger')
+            messages.error(request, "ログインしてください", extra_tags="danger")
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
@@ -132,13 +135,12 @@ class MakeFavoriteView(LoginRequiredMixin, View):
 
 
 class DestroyFavoriteView(LoginRequiredMixin, View):
-
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.error(request, "ログインしてください", extra_tags='danger')
+            messages.error(request, "ログインしてください", extra_tags="danger")
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
-        
+
     def post(self, request, *args, **kwargs):
 
         if request.is_ajax():
@@ -154,23 +156,21 @@ class DestroyFavoriteView(LoginRequiredMixin, View):
 
 class SignInView(AuthLoginView):
 
-    template_name = 'recipe/siteUser/signin.html'
+    template_name = "recipe/siteUser/signin.html"
     authentication_form = SignInForm
-    
+
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         if self.get_form().is_valid():
-            messages.success(request, 'ログインしました')
+            messages.success(request, "ログインしました")
         return response
 
+
 class SignOutView(AuthLogoutView):
-
-
-    @method_decorator(never_cache)
     def dispatch(self, request, *args, **kwargs):
-        
+
         response = super().dispatch(request, *args, **kwargs)
-        messages.success(request, 'ログアウトしました')
+        messages.success(request, "ログアウトしました")
         return response
 
 
@@ -178,8 +178,8 @@ class SignUpView(CreateView):
 
     model = SiteUser
     form_class = SignUpForm
-    success_url = reverse_lazy('recipe:random')
-    template_name = 'recipe/siteUser/signup.html'
+    success_url = reverse_lazy("recipe:random")
+    template_name = "recipe/siteUser/signup.html"
 
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
@@ -189,8 +189,8 @@ class SignUpView(CreateView):
             new_site_user.set_password(new_site_user.password)
             new_site_user.save()
             auth_login(request, new_site_user)
-            messages.success(self.request, 'アカウント登録が完了しました')
-        
+            messages.success(self.request, "アカウント登録が完了しました")
+
         return response
 
 
@@ -198,45 +198,38 @@ class UserPropertyChangeView(LoginRequiredMixin, UpdateView):
 
     model = SiteUser
     form_class = UserPropertyChangeForm
-    success_url = reverse_lazy('recipe:random')
-    template_name = 'recipe/siteUser/property-change.html'
+    success_url = reverse_lazy("recipe:random")
+    template_name = "recipe/siteUser/property-change.html"
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.error(request, "ログインしてください", extra_tags='danger')
+            messages.error(request, "ログインしてください", extra_tags="danger")
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         if self.get_form().is_valid():
-            messages.success(self.request, 'アカウント情報を変更しました')
-        
+            messages.success(self.request, "アカウント情報を変更しました")
+
         return response
 
 
 class PasswordEditView(LoginRequiredMixin, AuthPasswordChangeView):
 
     form_class = PasswordEditForm
-    template_name = 'recipe/siteUser/password-change.html'
-    success_url = reverse_lazy('recipe:random')
+    template_name = "recipe/siteUser/password-change.html"
+    success_url = reverse_lazy("recipe:random")
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.error(request, "ログインしてください", extra_tags='danger')
+            messages.error(request, "ログインしてください", extra_tags="danger")
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
-
 
     def post(self, request, *args, **kwargs):
         if self.get_form().is_valid():
             # パスワードが変更される前に先に実行
-            messages.success(self.request, 'パスワードの変更が完了しました')
+            messages.success(self.request, "パスワードの変更が完了しました")
         response = super().post(request, *args, **kwargs)
         return response
-
-
-
-
-
-
