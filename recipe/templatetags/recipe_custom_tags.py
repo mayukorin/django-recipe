24980f19ea9ef34ca63ce856e1ff97ab02726be3
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-03-29 12:33:01 +0900

    feat: add demo movie to README

--- recipe/templatetags/recipe_custom_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.template.defaultfilters import stringfilter
-
-register = template.Library()
-
-@register.filter
-@stringfilter
-def ingredient_img_path(value):
-    return "images/" + value + ".jpg"
